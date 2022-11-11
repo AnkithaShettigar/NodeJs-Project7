b47601af37fcb7710809a2b0ce31690a69294acc
AnkithaShettigar <ankitha.yermal@gmail.com>
2022-11-11 08:53:16 +0530

    Added Project7

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const data1 = require('./Routes/Bollywood');
+
+app.use('/home',data1)
+
+app.get('/',(req,res)=>{
+    res.write("hello");
+    res.end()
+})
+
+app.listen(process.env.PORT||8080,()=>{
+    console.log("server started...")
+})
