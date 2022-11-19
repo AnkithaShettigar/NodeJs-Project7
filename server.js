166f327ba3ec9716b1ce07927516e32bc9946d3e
AnkithaShettigar <ankitha.yermal@gmail.com>
2022-11-19 17:08:03 +0530

    Updated

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const data1 = require('./Routes/Bollywood');
 const cors = require('cors');
 
-app.use(cors);
+app.use(cors({
+    origin:"*",//most preferred
+}))
 
 app.use('/home',data1)
 
